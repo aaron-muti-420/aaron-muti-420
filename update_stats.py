aef5f2e29808551206e94c299f7c729224a40cd9
Aaron Markus <84517425+aaron-muti-420@users.noreply.github.com>
2023-10-13 12:16:45 +0200

    Update update_stats.py

--- update_stats.py
+++ update_stats.py
@@ -1,15 +1,23 @@
+import os
 import requests
 
-# Replace 'your_api_key' with your Wakatime API key
-api_key = 'waka_56b7b9cf-c4e8-454a-851f-27e0aac739b7'
+api_key = os.getenv('waka_56b7b9cf-c4e8-454a-851f-27e0aac739b7')  # Use an environment variable for the API key
 
-# Make a request to Wakatime's API to fetch statistics
-response = requests.get(f'https://wakatime.com/api/v1/users/current/stats/last_7_days?api_key={api_key}')
+if api_key:
+    try:
+        # Make a request to Wakatime's API to fetch statistics
+        response = requests.get(f'https://wakatime.com/api/v1/users/current/stats/last_7_days?api_key={api_key}')
+        response.raise_for_status()  # Raise an exception for 4xx/5xx status codes
 
-# Parse the response and extract relevant statistics
-data = response.json()
-total_coding_time = data['data']['total_seconds']
+        # Parse the response and extract relevant statistics
+        data = response.json()
+        total_coding_time = data['data']['total_seconds']
 
-# Update the README file with the statistics
-with open('README.md', 'a') as readme:
-    readme.write(f'Total Coding Time (Last 7 Days): {total_coding_time / 3600:.2f} hours\n')
+        # Update the README file with the statistics
+        with open('README.md', 'a') as readme:
+            readme.write(f'Total Coding Time (Last 7 Days): {total_coding_time / 3600:.2f} hours\n')
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+else:
+    print("Wakatime API key not found. Please set the WAKATIME_API_KEY environment variable.")
